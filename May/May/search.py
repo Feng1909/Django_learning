9038a3db822ce895b0c4f58e534a1ad9a3ac02ba
Feng Yunji <1120192186@bit.edu.cn>
2021-05-07 16:25:52 +0800

    I can transport images

--- May/May/search.py
+++ May/May/search.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import base64
+
 import os
 
 
@@ -13,6 +15,18 @@ def search(request):
     # request.encoding = 'utf-8'
     print("haha")
     print(request.FILES)
+    b64ImgData = request.FILES['image']
+    filename = request.FILES['tn']
+    # print(filename.read())
+
+    with open(filename.read(), 'wb') as f:
+        imgData = base64.b64decode(b64ImgData.read())
+        f.write(imgData)
+
+    # with open(filename, 'wb') as f:
+    #     imgData = base64.b64decode(b64ImgData)
+    #     f.write(imgData)
+
     if 'q' in request.GET and request.GET['q']:
         message = '你搜索的内容为: ' + request.GET['q']
     else:
--- test.py
+++ test.py
@@ -1,8 +1,16 @@
 import requests
+import base64
+import os
+
 url = "http://localhost:8000/search/"
+
+image_path = os.path.join(os.path.dirname(__file__))+'/test.jpg'
+with open(image_path, 'rb') as f:
+    imgData = base64.b64encode(f.read())
+
 files = {
-    "tn":"pc",
-    "image":("test.jpg",open('test.jpg','rb'),"image/jpeg"),
+    "tn":"pc.jpg",
+    "image": imgData,
     "from":"pc",
     "image_source":"PC_UPLOAD_SEARCH_FILE",
     "range":'{"page_from": "searchIndex"}'
