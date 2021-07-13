21e076d9597466df2b8cad3cbbeaecf3bec1e455
Feng Yunji <1120192186@bit.edu.cn>
2021-07-13 22:26:58 +0800

    add useless things

--- May/May/views.py
+++ May/May/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 def runoob(request):
   views_name = "菜鸟教程"
+  print("Successfully receive remote request")
   return  render(request,"runoob.html", {"name":views_name})
