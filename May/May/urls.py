1b3d89478048f6c63ebbe5876223df37af6997ce
Feng Yunji <1120192186@bit.edu.cn>
2021-07-24 13:24:09 +0800

    6

--- May/May/urls.py
+++ May/May/urls.py
@@ -1,20 +1,11 @@
-from blog import search
-from May import blog
 from django.conf.urls import include,url
-from blog import views
-from django.contrib import admin
+from . import views, search2
+#from ..blog import search
 
 urlpatterns = [
-    # url(r'^hello/$', views.runoob),
+    url(r'^hello/$', views.runoob),
     #url(r'^search-form/$', search.search_form),
-    # url(r'^search-post/$', search2.search_post),
-    # url(r'^',include('blog.urls')),
-    url(r'^', search.search),
-    url(r'^index/$',views.index),
-    #url(r'^(\d+)$',views.show),
-    url(r'^new/$',views.NewUser),
-    url(r'^delete/$',views.DeleteUser),
-    url(r'^modify/$',views.Modification),
-    url(r'^admin/',admin.site.urls),
+    url(r'^search-post/$', search2.search_post),
+    url(r'^',include('blog.urls')),
 
 ]
