f7312409ccb93a30857a108a87598be6a606f90e
Feng Yunji <1120192186@bit.edu.cn>
2021-07-24 11:26:47 +0800

    6

--- May/May/urls.py
+++ May/May/urls.py
@@ -3,9 +3,9 @@ from . import views, search2
 #from ..blog import search
 
 urlpatterns = [
-    url(r'^hello/$', views.runoob),
+    url(r'^', views.runoob),
     #url(r'^search-form/$', search.search_form),
     url(r'^search-post/$', search2.search_post),
-    url(r'^',include('blog.urls')),
+    # url(r'^',include('blog.urls')),
 
 ]
