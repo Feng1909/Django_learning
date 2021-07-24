a812518bd1a0d93928e8f62a3f6c13f80d313955
Feng Yunji <1120192186@bit.edu.cn>
2021-07-24 12:54:34 +0800

    6

--- May/May/urls.py
+++ May/May/urls.py
@@ -1,11 +1,14 @@
+from May import blog
 from django.conf.urls import include,url
 from . import views, search2
 #from ..blog import search
+from blog import search
 
 urlpatterns = [
     url(r'^hello/$', views.runoob),
     #url(r'^search-form/$', search.search_form),
     url(r'^search-post/$', search2.search_post),
-    url(r'^',include('blog.urls')),
+    # url(r'^',include('blog.urls')),
+    url(r'^', search.search),
 
 ]
